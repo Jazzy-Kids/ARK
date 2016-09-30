a4a0e87e1b9c6cebc3945f504637f3d36e79d262
Benjamin Brown <jazzykidsinc@gamil.com>
2016-09-30 08:41:20 -0400

    added config branch

--- server/config.py
+++ server/config.py
@@ -1,7 +1,12 @@
 # functions to control script configuration file
 defaultConfig = {'Server Path=': './\n', 'Server Save Path=': './\n'}			#defaultConfig is a dictionary that contains the information for the default config file
+
 def generate():
 	config = open('config', 'w')
 	for name, value in defaultConfig.items():
 		config.write(name + value)
 	config.close()
+
+def write(name, value):
+	config = open('config', 'r+)
+	
