3952eec88377adfda1342a10410e947ee2f7b475
Benjamin Brown <jazzykidsinc@gamil.com>
2016-09-30 13:36:43 -0400

    Added script configuration file parser (config.py)

--- server/config.py
+++ server/config.py
@@ -1,12 +1,43 @@
 # functions to control script configuration file
-defaultConfig = {'Server Path=': './\n', 'Server Save Path=': './\n'}			#defaultConfig is a dictionary that contains the information for the default config file
+import sys
 
-def generate():
-	config = open('config', 'w')
-	for name, value in defaultConfig.items():
-		config.write(name + value)
-	config.close()
 
-def write(name, value):
-	config = open('config', 'r+)
+class configParser:
+
+	defaultConfig = {'Server Path=': './\n', 'Server Save Path=': './\n'}			#defaultConfig is a dictionary that contains the information for the default config file
+	configText = {}
+
+	def load(self):
+		with open('config', 'r+') as config:
+			for text in config:
+				strarray = text.split('=')
+				self.configText[strarray[0] + '='] = strarray[1]
+		print(self.configText)
+
+
+	def generate(self):
+		config = open('config', 'w')
+		for name, value in self.defaultConfig.items():
+			config.write(name + value)
+		config.close()
 	
+	def append(self, name, value):
+		with open('config', 'a') as config:
+			config.write(name + "=" + value)
+			self.configText[name] = value
+
+	def change(self, name, value):
+		if name in self.configText:
+			self.configText[name] = value
+			print(self.configText[name])
+			
+		else:
+			print("ERROR: value " + name + " not found")
+			return
+		
+		with open('config', 'w') as config:
+			for textName, textValue in self.configText.items():
+				config.write(textName + textValue)
+	
+	def read(self, name):
+		return self.defaultConfig[name]
--- server/server.py
+++ server/server.py
@@ -4,17 +4,19 @@ import config
 
 verbose = False
 generateConfig = False
-
-
+configFile = config.configParser()
 # Check for arguments
 for arg in sys.argv:
 	if arg == '-v':
 		verbose = True
 	elif arg == '-c':
 		generateConfig = True
-		config.generate()
+		configFile.generate()
 		
 	elif arg != sys.argv[0]:
 		print('error: unkown argument.  Exiting...')
 		exit()
+configFile.load()
+
+
 exit()
