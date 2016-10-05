f6fc788d458688bd834ab7089989339cc0a4983e
Ben Brown <bbrown@bdnetcorp.com>
2016-10-05 14:09:04 -0400

    console

--- server/server.py
+++ server/server.py
@@ -5,6 +5,9 @@ import config
 verbose = False
 generateConfig = False
 configFile = config.configParser()
+
+helpText = "\nThis is a python script to manage an ark server.\n\n-h\tDisplays help text.\n-v\tVerbose mode(not working).\n-c\tGenerates a new config file for the script."
+
 # Check for arguments
 for arg in sys.argv:
 	if arg == '-v':
@@ -12,11 +15,21 @@ for arg in sys.argv:
 	elif arg == '-c':
 		generateConfig = True
 		configFile.generate()
+	elif arg == '-h':
+		print(helpText)
+		exit()
 		
 	elif arg != sys.argv[0]:
 		print('error: unkown argument.  Exiting...')
 		exit()
 configFile.load()
 
+print("Welcome to the ark management script")
+command = input()
+command = command.upper()
+
+if command == "QUIT" or command == "Q":
+	print('\nQuitting...')
+	exit()	
 
 exit()
